9934221040386535656628e99274a2c8788bb7d1
Cecilia ELkrieff <cecilia.elkrieff@gmail.com>
2026-02-04 17:40:16 +0100

    security: ajout config email avec exemple

---
+++ App/Config/email.example.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'smtp_host' => 'smtp.gmail.com',
+    'smtp_port' => 587,
+    'smtp_username' => 'votre.email@gmail.com',
+    'smtp_password' => 'votre_mot_de_passe_app',
+    'smtp_from' => 'votre.email@gmail.com',
+    'smtp_from_name' => 'EventHub',
+    'smtp_to' => 'votre.email@gmail.com'
+];
